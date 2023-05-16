31401917fc04f3469f81fdc755439cf7541d8f85
Greg Field <greg.field1992@gmail.com>
2023-05-16 23:50:52 +0100

    add delete task and placeholder text and fix delete list

--- pages/todo.vue
+++ pages/todo.vue
@@ -10,7 +10,7 @@ function newList() {
 }
 function deleteList(index) {
     console.log('delete list ', index)
-    lists.splice(0, index)
+    lists.splice(index, 1)
 }
 
 function addTask(list) {
@@ -20,6 +20,20 @@ function addTask(list) {
         list.tasks.push({
             name: 'task'
         })
+    }
+}
+
+function deleteTask(list, index) {
+    if (list.tasks) {
+        list.tasks.splice(index, 1)
+    } 
+}
+
+function showDesc(task) {
+    if (!task.showDescription) {
+        task.showDescription = true;
+    } else {
+        task.showDescription = false
     }
 }
 
@@ -34,13 +48,28 @@ function addTask(list) {
     </v-card>
 
     <v-card :variant="variant" v-for="(list, index) in lists">
-        <v-text-field></v-text-field>
-        <v-btn @click="addTask(list)">New Task</v-btn>
-        <v-btn @click="deleteList(index)">Delete</v-btn>
-
+        <v-cart-title>
+            <v-text-field placeholder="My List"></v-text-field>
+        </v-cart-title>
+        <v-card-actions>
+            <v-btn color="primary" @click="addTask(list)">New Task</v-btn>
+            <v-btn color="secondary" @click="deleteList(index)">Delete</v-btn>
+        </v-card-actions>
+        <v-col>
         <v-card variant="outlined" v-for="(task, index) in list.tasks">
-            <v-text-field></v-text-field>
+            <v-cart-title>
+            <v-text-field placeholder="My Task"></v-text-field>
+        </v-cart-title>
+        
+        <v-card-text>
+            <!-- <v-icon @click="showDesc(task)">mdi-plus</v-icon> -->
+            <!-- <v-textarea v-if="task.showDescription" clearable label="" variant="solo-inverted"></v-textarea> -->
+        </v-card-text>
             <v-checkbox></v-checkbox>
+            <v-card-actions>
+                <v-btn color="secondary" @click="deleteTask(list, index)">Delete</v-btn>
+            </v-card-actions>
         </v-card>
+    </v-col>
     </v-card>
 </template>
