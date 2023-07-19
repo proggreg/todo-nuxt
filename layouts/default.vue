92b40dd5d19da3e5c40cad85f14cc383c7be35e4
Greg Field <greg.field1992@gmail.com>
2023-07-19 22:46:06 +0100

    clean up

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,4 @@
-import { ref } from 'vue'
 <script setup>
-// you don't need this: only for testing purposes
-const date = useAppConfig().buildDate
 let darkMode = 'light';
 if (process.client && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     // dark mode
@@ -51,67 +48,6 @@ function toggleDarkMode(params) {
         <slot />  
       </v-container>
       </v-main>
-  
-      <!-- TODO finish bottom nav -->
-    <!-- <v-bottom-navigation :elevation="0" grow>
-  <v-btn value="recent">
-    <v-icon>mdi-history</v-icon>
-
-    Recent
-  </v-btn>
-
-  <v-btn value="favorites">
-    <v-icon>mdi-heart</v-icon>
-
-    Favorites
-  </v-btn>
-
-  <v-btn value="nearby">
-    <v-icon>mdi-map-marker</v-icon>
-
-    Nearby
-  </v-btn>
-</v-bottom-navigation> -->
-<ClientOnly>
-      <v-card
-        v-if="$pwa?.offlineReady || $pwa?.needRefresh"
-      >
-        <div class="message">
-          <span v-if="$pwa.offlineReady">
-            App ready to work offline
-          </span>
-          <span v-else>
-            New content available, click on reload button to update.
-          </span>
-        </div>
-        <button
-          v-if="$pwa.needRefresh"
-          @click="$pwa.updateServiceWorker()"
-        >
-          Reload
-        </button>
-        <button @click="$pwa.cancelPrompt()">
-          Close
-        </button>
-      </v-card>
-      <v-card
-        v-if="$pwa?.showInstallPrompt && !$pwa?.offlineReady && !$pwa?.needRefresh"
-        class="pwa-toast"
-        role="alert"
-      >
-        <div class="message">
-          <span>
-            Install PWA
-          </span>
-        </div>
-        <v-btn @click="$pwa.install()">
-          Install
-        </v-btn>
-        <v-btn variant="tonal" @click="$pwa.cancelInstall()">
-          Cancel
-        </v-btn>
-      </v-card>
-</ClientOnly>
   </v-layout>
   </v-app>
 </v-theme-provider>
@@ -120,26 +56,4 @@ function toggleDarkMode(params) {
   </main>
 </template>
 
-<style>
-.pwa-toast {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  margin: 16px;
-  padding: 12px;
-  border: 1px solid #8885;
-  border-radius: 4px;
-  z-index: 1;
-  text-align: left;
-}
-.pwa-toast .message {
-  margin-bottom: 8px;
-}
-.pwa-toast button {
-  border: 1px solid #8885;
-  outline: none;
-  margin-right: 5px;
-  border-radius: 2px;
-  padding: 3px 10px;
-}
-</style>
+
