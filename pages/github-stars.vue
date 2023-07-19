51b79da044b60a6aeeb431aa2457907ac65cd56b
Greg Field <greg.field1992@gmail.com>
2023-07-19 22:46:23 +0100

    add description

--- pages/github-stars.vue
+++ pages/github-stars.vue
@@ -1,12 +1,47 @@
 <script setup>
-let url = 'https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&order=desc'
+let url = 'https://api.github.com/search/repositories'
 let pageNumber = ref(1)
 let perPage = ref('12')
 let repos = reactive([])
+let language = ref('')
+const programmingLanguages = ref([
+    "Python",
+    "JavaScript",
+    "Java",
+    "C++",
+    "C#",
+    "Ruby",
+    "Swift",
+    "Go",
+    "PHP",
+    "TypeScript",
+    "R",
+    "Kotlin",
+    "Scala",
+    "Rust",
+    "Perl",
+    "Objective-C",
+    "Lua",
+    "Haskell",
+    "MATLAB",
+    "Shell"
+]);
+let query = 'stars';
 let {data, refresh} = await useFetch(url, {
   transform: (data) => data.items,
-  query: { page: pageNumber, per_page: perPage}
+  query: { 
+    page: pageNumber,
+    per_page: perPage,
+    sort: 'stars',
+    order: 'desc',
+    q: query, 
+    }
 })
+
+if (process.client) {
+  console.log(JSON.stringify(data.value))
+  window.repos = data.value
+}
 
 const pageOptions = ['12', '24', '48']
 function backPage() {
@@ -25,18 +60,19 @@ function nextPage() {
 </script>
 <template>
 <v-row>
-  <v-col v-for="repo in data" cols="4" >
+  <v-col v-for="repo in data" cols="12" xs="6" sm="6" md="4" >
     <v-card :title="repo.name" >
-            <v-card-text >
-              {{ repo.stargazers_count }}
-  
-        <v-icon icon="mdi-star" color="yellow"/>
+      <v-card-text class="line-clamp">         
+          {{ repo.description }}
       </v-card-text>
-      <v-card-action>
+      <v-card-actions>
         <a :href="repo.html_url" target="_blank">
         <v-btn>Open</v-btn>
             </a>
-      </v-card-action>
+            <a :href="repo.stargazers_url">
+              {{ repo.stargazers_count }} <v-icon icon="mdi-star" color="yellow"/>
+            </a>
+          </v-card-actions>
     </v-card>
   </v-col>
 </v-row>
@@ -62,6 +98,27 @@ function nextPage() {
         </v-list-item>
       </v-list>
     </v-menu>
+
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          v-bind="props"
+        >
+          {{ language }}
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(option, index) in programmingLanguages"
+          :key="index"
+          :value="option"
+          @click="query = 'stars&language='+ option;console.log(query); refresh()"
+        >
+          <v-list-item-title>{{ option }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-col>
   <v-col>
     {{ pageNumber }}
@@ -75,3 +132,11 @@ function nextPage() {
 </v-row>
 
 </template>
+<style>
+.line-clamp {
+  display: -webkit-box;
+  -webkit-line-clamp: 8;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
