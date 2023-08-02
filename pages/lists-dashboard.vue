ee45121d817008dc9f88d3856bb67f9cbde6e67c
Greg Field <greg.field1992@gmail.com>
2023-08-02 08:58:06 +0100

    fix col layout

--- pages/lists-dashboard.vue
+++ pages/lists-dashboard.vue
@@ -24,37 +24,39 @@ function updateDrawer (el) {
 }
 </script>
 <template>
-  <v-navigation-drawer
-    app
-    class="pa-2 fill-height rounded-lg d-none d-md-flex"
-    @update:rail="updateDrawer"
-  >
-    <v-list>
-      <v-list-item>
-        <template #prepend>
-          <v-btn rounded @click="newList">
-            <template #prepend>
-              <v-icon>mdi-plus</v-icon>
-            </template>
-            New List
-          </v-btn>
-        </template>
-      </v-list-item>
-    </v-list>
-    <v-divider />
-    <app-lists v-if="data" :lists="data" />
-  </v-navigation-drawer>
-  <v-col class="rounded-lg d-lg-none" cols="1">
-    <v-btn class="" no-gutters @click="openMobileNav">
-      Lists
-    </v-btn>
-  </v-col>
-  <v-col>
-    <app-list />
-  </v-col>
-  <v-col class="fill-height">
-    <v-sheet class="fill-height rounded-lg">
-      <app-list-item />
-    </v-sheet>
-  </v-col>
+  <v-row>
+    <v-navigation-drawer
+      app
+      class="pa-2 fill-height rounded-lg d-none d-md-flex"
+      @update:rail="updateDrawer"
+    >
+      <v-list>
+        <v-list-item>
+          <template #prepend>
+            <v-btn rounded @click="newList">
+              <template #prepend>
+                <v-icon>mdi-plus</v-icon>
+              </template>
+              New List
+            </v-btn>
+          </template>
+        </v-list-item>
+      </v-list>
+      <v-divider />
+      <app-lists v-if="data" :lists="data" />
+    </v-navigation-drawer>
+    <v-col class="rounded-lg d-lg-none" cols="1">
+      <v-btn class="" no-gutters @click="openMobileNav">
+        Lists
+      </v-btn>
+    </v-col>
+    <v-col cols="6">
+      <app-list />
+    </v-col>
+    <v-col class="fill-height" cols="6">
+      <v-sheet class="fill-height rounded-lg">
+        <app-list-item />
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
