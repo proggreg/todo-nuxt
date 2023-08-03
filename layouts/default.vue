d100db2c26def5664ec2fb7f7e27f148cc98e586
Greg Field <greg.field1992@gmail.com>
2023-08-03 19:11:04 +0100

    fix dark mode theme change

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,18 +5,26 @@
         <v-layout>
           <v-app-bar :elevation="0">
             <NuxtLink to="/about" custom>
-              <v-btn class="btn pl-1" to="/"> Proggreg </v-btn>
+              <v-btn class="btn pl-1" to="/">
+                Proggreg
+              </v-btn>
             </NuxtLink>
 
             <NuxtLink v-slot="{ navigate }" to="/about" custom>
-              <v-btn class="d-none d-sm-flex" @click="navigate"> about </v-btn>
+              <v-btn class="d-none d-sm-flex" @click="navigate">
+                about
+              </v-btn>
             </NuxtLink>
 
             <template #append>
               <v-switch inset hide-details="auto" @click="toggleDarkMode">
                 <template #prepend>
-                  <v-icon v-if="theme === 'light'">mdi-weather-night</v-icon>
-                  <v-icon v-else>mdi-weather-sunny</v-icon>
+                  <v-icon v-if="theme === 'light'">
+                    mdi-weather-night
+                  </v-icon>
+                  <v-icon v-else>
+                    mdi-weather-sunny
+                  </v-icon>
                 </template>
               </v-switch>
             </template>
@@ -32,23 +40,24 @@
   </main>
 </template>
 <script setup>
-let darkMode = "myCustomLightTheme";
+let darkMode = 'myCustomLightTheme'
 if (
   process.client &&
   window.matchMedia &&
-  window.matchMedia("(prefers-color-scheme: dark)").matches
+  window.matchMedia('(prefers-color-scheme: dark)').matches
 ) {
   // dark mode
-  darkMode = "dark";
+  darkMode = 'myCustomDarkTheme'
 }
 
-const theme = ref(darkMode);
+const theme = ref(darkMode)
 
-function toggleDarkMode() {
-  if (theme.value === "myCustomDarkTheme") {
-    theme.value = "myCustomLightTheme";
+function toggleDarkMode () {
+  console.log('Toggling dark mode', theme.value)
+  if (theme.value === 'myCustomDarkTheme') {
+    theme.value = 'myCustomLightTheme'
   } else {
-    theme.value = "myCustomDarkTheme";
+    theme.value = 'myCustomDarkTheme'
   }
 }
 </script>
