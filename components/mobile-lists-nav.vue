54925868c34da5634bc8eb9a90e9881532233bbc
Greg Field <greg.field1992@gmail.com>
2023-08-04 08:48:13 +0100

    lint

--- components/mobile-lists-nav.vue
+++ components/mobile-lists-nav.vue
@@ -1,25 +1,25 @@
 <script setup>
-import { defineProps, defineEmits } from "vue";
+import { defineProps, defineEmits } from 'vue'
 
-import { useListsStore } from "~/stores/lists";
+import { useListsStore } from '~/stores/lists'
 const props = defineProps({
   open: Boolean,
   lists: {
     type: Array,
-    default() {
-      return [];
-    },
-  },
-});
-const emit = defineEmits(["close"]);
-const listsStore = useListsStore();
+    default () {
+      return []
+    }
+  }
+})
+const emit = defineEmits(['close'])
+const listsStore = useListsStore()
 
-function newList() {
-  listsStore.addList();
-  emit("close", false);
+function newList () {
+  listsStore.addList()
+  emit('close', false)
 }
-function selectList() {
-  emit("close", false);
+function selectList () {
+  emit('close', false)
 }
 </script>
 <template>
@@ -27,10 +27,12 @@ function selectList() {
     <list-items
       :lists="listsStore.lists"
       @select-list="selectList"
-    ></list-items>
+    />
     <template #append>
       <div class="pa-2">
-        <v-btn @click="newList">New List</v-btn>
+        <v-btn @click="newList">
+          New List
+        </v-btn>
       </div>
     </template>
   </v-navigation-drawer>
