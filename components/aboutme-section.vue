598e83fdd77ddb89cd8ed4590009b29a9163837f
Greg Field <greg.field1992@gmail.com>
2023-08-17 21:43:22 +0100

    add section

---
+++ components/aboutme-section.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-sheet class="fill-height">
+    <h1>I'm Greg</h1>
+    <v-row>
+      <v-col>
+        <p>A software developer who takes delight in tackling challenges through problem-solving and analytical thought processes.</p>
+        <p>I really like picking up new tech stuff and just making things with it.</p>
+      </v-col>
+    </v-row>
+  </v-sheet>
+</template>
