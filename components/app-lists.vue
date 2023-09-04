28a3a9703319f6c16a269fd957be975d05adfb0c
Greg Field <greg.field1992@gmail.com>
2023-09-04 21:29:04 +0100

    refactor to use pinia store

--- components/app-lists.vue
+++ components/app-lists.vue
@@ -1,26 +1,23 @@
 <script setup>
 import { useListsStore } from '~/stores/lists'
-const { data: lists } = await useFetch('/api/lists')
 const listsStore = useListsStore()
-const emit = defineEmits(['selectList'])
-
-if (lists) {
-  listsStore.setLists(lists)
-}
 
 function selectList (list) {
   listsStore.setCurrentList(list)
-  emit('selectList', list)
+  listsStore.getTodos(list._id)
 }
+onMounted(() => {
+  listsStore.getLists() // TODO get on server (useFetch)
+})
 </script>
 
 <template>
   <v-list nav>
-    <v-list-item v-if="!lists || !lists.length">
+    <v-list-item v-if="!listsStore.lists || !listsStore.lists.length">
       <v-list-item-title>No lists yet</v-list-item-title>
     </v-list-item>
     <v-list-item
-      v-for="(list, i) in lists"
+      v-for="(list, i) in listsStore.lists"
       v-else
       :key="i"
       color="accent"
@@ -31,6 +28,9 @@ function selectList (list) {
       @click="selectList(list)"
     >
       <v-list-item-title>{{ list.name }}</v-list-item-title>
+      <template #append>
+        <options-menu :list-id="list._id" />
+      </template>
     </v-list-item>
   </v-list>
 </template>
