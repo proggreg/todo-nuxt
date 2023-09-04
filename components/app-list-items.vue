0807e2383113a2927a832c87c330e00e297e7fb4
Greg Field <greg.field1992@gmail.com>
2023-09-04 21:23:27 +0100

    add typescript and select todo

--- components/app-list-items.vue
+++ components/app-list-items.vue
@@ -1,61 +1,67 @@
-<script setup>
+<script setup lang="ts">
 import { useListsStore } from '~/stores/lists'
 const todoName = ref('')
 const listsStore = useListsStore()
-const url = computed(() => `/api/list/todo/${listsStore.currentList._id}`)
-const { data: todos, refresh } = await useFetch(url)
 
-async function addTodo () {
-  if (todoName.value) {
-    await $fetch(`/api/list/todo/${listsStore.currentList._id}`, {
-      method: 'POST',
-      body: {
-        name: todoName.value,
-        done: false,
-        list_id: listsStore.currentList._id
-      }
-    })
-    refresh()
+interface Todo {
+  name: string;
+  done: boolean;
+  _id?: string;
+}
+
+onMounted(() => {
+  if (listsStore.currentList && listsStore.currentList._id) {
+    listsStore.getTodos(listsStore.currentList._id)
+  }
+})
+
+function addTodo () {
+  if (todoName.value && listsStore.currentList) {
+    listsStore.addTodo(todoName.value)
   }
 
   todoName.value = ''
 }
 
-async function deleteTodo (todo) {
+async function deleteTodo (todo: Todo) {
   await $fetch(`/api/list/todo/${todo._id}`, {
     method: 'DELETE'
   })
-  refresh()
 }
 
-function editTodo (todo) {
+function editTodo (todo: Todo) {
   todo.done = !todo.done
   $fetch(`/api/list/todo/${todo._id}`, {
     method: 'PUT',
     body: todo
   })
 }
+function selectTodo (todo: Todo) {
+  listsStore.setCurrentTask(todo)
+}
 
 </script>
 
 <template>
   <v-text-field
+    v-if="listsStore.currentList"
     v-model="todoName"
     variant="solo-filled"
     rounded
-    :placeholder="'Add todo to 2' + listsStore.currentList.name"
+    :placeholder="'Add todo to ' + listsStore.currentList.name"
     @keyup.enter="addTodo()"
   />
-  <v-list elevation="0" rounded>
+  <v-list v-if="listsStore.currentList" elevation="0" rounded>
     <v-list-subheader>Todo</v-list-subheader>
     <v-list-item
-      v-for="(todo, index) in todos"
+      v-for="(todo, index) in listsStore.currentList.todos"
       :key="index"
       density="compact"
       variant="text"
     >
       <v-list-item-title
         :class="todo.done ? 'text-decoration-line-through' : ''"
+        @click="selectTodo(todo)"
       >
         {{ todo.name }}
       </v-list-item-title>
