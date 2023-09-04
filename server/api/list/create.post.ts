66b3260f403fd780a692459175a36fdc33a203f7
Greg Field <greg.field1992@gmail.com>
2023-09-04 21:28:42 +0100

    remove log

--- server/api/list/create.post.ts
+++ server/api/list/create.post.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   try {
+    console.log('create list ', body)
     return await new ListSchema(body).save()
   } catch (error) {
     console.error('here', error)
