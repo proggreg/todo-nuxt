5722af3f2fc095097393d79d126d8e933650c747
Greg Field <greg.field1992@gmail.com>
2023-09-04 21:29:54 +0100

    refactor to use store

--- components/app-list.vue
+++ components/app-list.vue
@@ -1,36 +1,31 @@
-<script setup>
+<script setup lang="ts">
 import { useListsStore } from '~/stores/lists'
-const listsStore = useListsStore()
+const store = useListsStore()
+const list = computed(() => store.currentList)
 
-const emit = defineEmits(['todoSelected'])
-
-function updateListName () { // TODO use edit button to edit title
-  if (listsStore.currentList.name) {
-    $fetch('/api/list/update', {
-      method: 'PUT',
-      body: {
-        name: listsStore.currentList.name,
-        _id: listsStore.listId
-      }
-    })
-  }
-}
+// function updateListName () { // TODO use edit button to edit title
+//   if (store.currentList.name) {
+//     $fetch('/api/list/update', {
+//       method: 'PUT',
+//       body: {
+//         name: store.currentList.name,
+//         _id: store.listId
+//       }
+//     })
+//   }
+// }
 </script>
 <template>
-  <v-row v-if="listsStore.currentList" no-gutters>
-    <v-col cols="12">
+  <v-row v-if="list" no-gutters>
+    <v-col class="py-4" cols="12">
       <h1>
-        {{ listsStore.currentList.name }}
+        {{ list.name }}
       </h1>
     </v-col>
     <v-col>
       <app-list-items
-        v-if="listsStore.currentList"
-        @select-todo="emit('todoSelected')"
+        v-if="list"
       />
-    </v-col>
-    <v-col cols="2" class="pa-4">
-      <list-delete-button :_id="listsStore.currentList._id" />
     </v-col>
   </v-row>
 </template>
