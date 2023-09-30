a96fa24538b394c379c6cf048f0da1591475dca8
Greg Field <greg.field1992@gmail.com>
2023-09-30 12:48:17 +0100

    remove typo

--- stores/lists.ts
+++ stores/lists.ts
@@ -11,7 +11,7 @@ export const useListsStore = defineStore('lists', {
   state: () : listsState => ({
     lists: [],
     currentList: undefined,
-    currentTask: undefinedx§
+    currentTask: undefined
   }),
   actions: {
     async addList (listName: string) {
