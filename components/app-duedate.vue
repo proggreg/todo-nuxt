a4f41499e6ce77456e1867dea350cc42fed7934a
Greg Field <greg.field1992@gmail.com>
2023-10-01 17:52:52 +0100

    add duedate component

--- components/app-duedate.vue
+++ components/app-duedate.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
-const store = useListsStore()
+const emit = defineEmits(['setDate'])
+
 const date = ref(new Date())
 
-const format = (date: Date) => {
-  const day = date.getDate()
-  const month = date.getMonth() + 1
-  const year = date.getFullYear()
-
-  return `${day}/${month}/${year}`
-}
-
-function setDueDate () {
-  if (date.value) {
-    store.setDueDate(date.value)
-  }
-}
+watch(date, (newDate) => {
+  emit('setDate', newDate)
+})
 
 </script>
 <template>
-  <VueDatePicker v-model="date" :format="format" @closed="setDueDate" />
+  <VueDatePicker
+    v-model="date"
+    :enable-time-picker="false"
+  />
 </template>
