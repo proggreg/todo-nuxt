0c90dca71adf1ab56158cdd3810647ad193656ea
Greg Field <greg.field1992@gmail.com>
2023-10-01 20:56:05 +0100

    today page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-import { Todo } from '~/types/globals'
-const todaysTodos = ref<Todo[]>([])
+
 </script>
 
 <template>
-  <v-list title="Today">
-    <v-list-title>Today</v-list-title>
-    <v-list-item v-for="todo in todaysTodos" :key="todo.name">
-      {{ todo.name }}
-    </v-list-item>
-  </v-list>
+  <h1>Today</h1>
+  <app-list-items />
 </template>
