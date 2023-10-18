854bb88648ce75ead6376b13774e577f167ce75b
Greg Field <greg.field1992@gmail.com>
2023-10-18 08:12:23 +0100

    fix / refactor dark mode toggle

--- components/darkmode-switch.vue
+++ components/darkmode-switch.vue
@@ -1,28 +1,15 @@
-<script setup>
-let darkMode = 'myCustomLightTheme'
-if (
-  process.client &&
-  window.matchMedia &&
-  window.matchMedia('(prefers-color-scheme: dark)').matches
-) {
-  // dark mode
-  darkMode = 'myCustomDarkTheme'
-}
-
-const theme = ref(darkMode)
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+const theme = useTheme()
 
 function toggleDarkMode () {
-  if (theme.value === 'myCustomDarkTheme') {
-    theme.value = 'myCustomLightTheme'
-  } else {
-    theme.value = 'myCustomDarkTheme'
-  }
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
 }
-</script>>
+</script>
 <template>
-  <v-switch inset hide-details="auto" @click="toggleDarkMode">
+  <v-switch inset :hide-details="true" @click="toggleDarkMode">
     <template #prepend>
-      <v-icon v-if="theme === 'light'">
+      <v-icon v-if="theme.global.name.value === 'light'">
         mdi-weather-night
       </v-icon>
       <v-icon v-else>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
-const theme = ref('myCustomLightTheme')
 </script>
 <template>
   <VitePwaManifest />
   <main>
-    <v-theme-provider :theme="theme" with-background>
-      <v-app>
-        <app-nav />
+    <v-app>
+      <app-nav />
 
-        <v-main>
-          <v-container fluid>
-            <NuxtPage />
-          </v-container>
-        </v-main>
-      </v-app>
-    </v-theme-provider>
+      <v-main>
+        <v-container fluid>
+          <NuxtPage />
+        </v-container>
+      </v-main>
+    </v-app>
   </main>
 </template>
