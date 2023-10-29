abbff3bc4d30a630abf35a8f24268741a3984083
Greg Field <greg.field1992@gmail.com>
2023-10-29 10:36:42 +0000

    today dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,13 @@
 import { Todo } from '@/types/globals'
 const listsStore = useListsStore()
 listsStore.getTodaysTodos()
-const newTodo = ref<Todo>({
-  name: '',
-  done: false
-})
-function addTodo () {
-  newTodo.value.dueDate = new Date()
-  if (newTodo && listsStore.currentList) {
-    listsStore.addTodo(newTodo.value)
-  }
 
-  newTodo.value.name = ''
-  newTodo.value.done = false
-  newTodo.value.dueDate = undefined
+function todoSelected () {
+  console.log('selected')
 }
+
 </script>
 
 <template>
-  <h1>Today</h1>
-
-  <v-text-field
-    v-if="listsStore.currentList"
-    v-model="newTodo.name"
-    variant="solo-filled"
-    rounded="lg"
-    :placeholder="'Add todo to ' + listsStore.currentList.name"
-    class="add-todo-field"
-    @keyup.enter="addTodo()"
-  />
-
-  <app-list-items />
+  <app-list @todo-Selected="todoSelected" />
 </template>
