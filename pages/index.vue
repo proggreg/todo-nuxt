8591e59d4413217f446ee95cebbc7b8cdfd93ad6
Greg Field <greg.field1992@gmail.com>
2023-11-03 08:35:34 +0000

    move statuses

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 const listsStore = useListsStore()
-const statuses = reactive(['Open', 'Done'])
+
 
 </script>
 
 <template>
   <app-list>
-    <app-list-items v-if="listsStore.todaysTodos" :statuses="statuses" :todos="listsStore.todaysTodos" list-name="Today" />
+    <app-list-items v-if="listsStore.todaysTodos"  :todos="listsStore.todaysTodos" list-name="Today" />
   </app-list>
 </template>
