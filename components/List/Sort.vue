1c7f869c938ae3806e6c1bb509180c56d44e424f
Greg Field <greg.field1992@gmail.com>
2023-11-24 08:30:20 +0000

    add sort component

---
+++ components/List/Sort.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const selectedSort = ref(null)
+const sortOptions = ref([
+  { text: 'Default', value: 'default' },
+  { text: 'Priority', value: 'priority' },
+  { text: 'Due Date', value: 'dueDate' }
+])
+</script>
+
+<template>
+  <v-select
+    v-model="selectedSort"
+    :items="sortOptions"
+    item-title="text"
+    item-value="value"
+    label="Sort By"
+    outlined
+  />
+</template>
